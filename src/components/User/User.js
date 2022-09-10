5c7b79a57273ff32eadd2e86d3e8fe20a9a2ffbd
Mohammed-mehdi-raza <mehdirazajafri2040@gmail.com>
2022-09-10 17:02:58 +0530

    first

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -11,8 +11,8 @@ function User(){
                 </div>
                 <br/>
                 <div >
-                    <Button variant="outlined" href="http://localhost:3000/user/student">Student</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button variant="outlined" href="http://localhost:3000/user/admin">Admin</Button>
+                    <Button variant="outlined" href="https://cool-frangipane-8ecd98.netlify.app/user/student">Student</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <Button variant="outlined" href="https://cool-frangipane-8ecd98.netlify.app/user/admin">Admin</Button>
                 </div>
             </div>
         </div>
